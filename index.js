a55d1781592fb8550d91cdf09e9a40227b09c933
Slavo <75747701+SlavoKosta@users.noreply.github.com>
2020-12-27 13:09:19 -0700

    Added touch section to JS

---
+++ index.js
@@ -0,0 +1,226 @@
+
+let shapes =[]
+let labels = []
+let positives = 0
+let negatives = 0
+let score = 0
+
+setWelcome()
+
+function setWelcome(){
+    let dWelcome = document.createElement('div')
+    let hWelcome = document.createElement('h1')
+    let btnWelcome = document.createElement('button')
+
+    document.querySelector('.intro').classList.add('welcome')
+    btnWelcome.innerHTML = "OK"
+    btnWelcome.classList.add('btn')
+    hWelcome.innerText = "Pair Shape to Label"
+
+    dWelcome.appendChild(hWelcome)
+    dWelcome.appendChild(btnWelcome)
+    dWelcome.classList.add('welcomeWindow')
+
+    document.querySelector('.intro').appendChild(dWelcome)
+
+    btnWelcome.addEventListener('click', () => {
+        document.querySelector('.intro').classList.remove('welcome')
+        document.querySelector('.intro').innerHTML= ''
+        setScore(score)
+        setTask()
+    })
+}
+
+function setRandoms(){
+    const names = ['circle','square','triangle']
+    const colors = ['yellow','blue','limegreen']
+    let randomNames = shuffle(names)
+    let randomColors = shuffle(colors)
+
+    for(i=0; i<3; i++){
+        let shape = {
+            name: randomNames[i],
+            color: randomColors[i]
+        }
+        shapes.push(shape)
+    }
+
+    randomNames = shuffle(names)
+    randomColors = shuffle(colors)
+
+    for(i=0; i<3; i++){
+        let label = {
+            name: randomNames[i],
+            color: randomColors[i]
+        }
+        labels.push(label)
+    }
+}
+
+let Xs = []
+let pShapes = []
+let pLabels = []
+
+function setTask(){    
+    document.querySelector('.task').innerHTML=''
+    document.querySelector('.texts').textContent=''
+    shapes = []
+    labels = []
+    pShapes = []
+    pLabels = []
+
+    setRandoms()
+
+    for(i=0; i<3;i++){
+        pShapes[i] = document.createElement('p')
+        pShapes[i].draggable = true
+        pShapes[i].classList.add('draggable')
+        pShapes[i].classList.add(shapes[i].name)
+        pShapes[i].setAttribute('name',shapes[i].name)
+        document.querySelector('.task').appendChild(pShapes[i])
+        var c = document.querySelector(`.${shapes[i].name}`)        
+        c.style.backgroundColor = shapes[i].color
+       
+        let boxS = pShapes[i].getBoundingClientRect()
+        Xs.push(boxS.left + boxS.width/2) 
+    }
+
+    for(i=0; i<3;i++){
+        pLabels[i] = document.createElement('p')
+        pLabels[i].draggable = true
+        pLabels[i].classList.add('draggable')
+        pLabels[i].innerHTML=labels[i].name
+        pLabels[i].setAttribute('name',labels[i].name)
+        pLabels[i].classList.add('text')
+        pLabels[i].style.color = labels[i].color
+        pLabels[i].style.border = `2px solid ${labels[i].color}`
+        document.querySelector('.texts').appendChild(pLabels[i])
+
+        let boxT = pLabels[i].getBoundingClientRect()
+        boxT.left = Xs[i]
+    }
+
+    const draggables = document.querySelectorAll('.draggable')
+
+    draggables.forEach(draggable => {
+        draggable.addEventListener('dragstart',() => {
+            draggable.classList.add('dragging')
+        })
+
+        draggable.addEventListener('dragend', (e) => {
+            check(e)
+            draggable.classList.remove('dragging')
+        })
+
+        draggable.addEventListener('dragover', (e) => {
+            e.preventDefault()
+        })
+
+        /* Mobile screen */
+
+        draggable.addEventListener('touchstart',() => {
+            draggable.classList.add('dragging')
+        })
+
+        draggable.addEventListener('touchend', (e) => {
+            check(e)
+            draggable.classList.remove('dragging')
+        })
+
+        draggable.addEventListener('touchmove', (e) => {
+            e.preventDefault()
+        })
+
+
+    })
+}
+
+function check(e){
+    let dragElement = document.querySelector('.dragging')
+    let dragElementRect = dragElement.getBoundingClientRect()
+
+    if(e.clientY>=dragElementRect.bottom){
+        for(i=0; i<3; i++){
+        let pLx = pLabels[i].getBoundingClientRect()
+            if(e.clientX >= pLx.left && e.clientX <= pLx.left + pLx.width){
+                if(dragElement.getAttribute('name') === pLabels[i].getAttribute('name')){
+                    setWon()
+                    removePair(dragElement, pLabels[i], 1)
+                }else{
+                    setFail()
+                }
+                break
+            }
+        } 
+    }
+
+    if(e.clientY<=dragElementRect.top){
+        for(i=0; i<3; i++){
+        let pSx = pShapes[i].getBoundingClientRect()
+            if(e.clientX >= pSx.left && e.clientX <= pSx.left + pSx.width){
+                if(dragElement.getAttribute('name') === pShapes[i].getAttribute('name')){
+                    setWon()
+                    removePair(dragElement, pShapes[i], -1)
+                }else{
+                    setFail()
+                }
+                break
+            }
+        } 
+    }
+}
+
+function removePair(dragged, matched, f){
+    positives++
+    if(positives < 2){        
+        dragged.className = ''
+        matched.className = ''
+        dragged.innerHTML = ''
+        matched.innerHTML = ''
+        dragged.style = ''
+        matched.style = ''
+    }else{
+        positives = 0
+        setTask()
+    }
+    score++
+    setScore(score)
+}
+
+function setFail(){
+    let dFail = document.createElement('div')
+    dFail.classList.add('fail')
+    dFail.style.opacity = 0.8
+    document.querySelector('.lost').appendChild(dFail)
+    setTimeout(() => document.querySelector('.lost').innerHTML='', 100)
+    setTask()
+    score = 0
+    setScore(score)
+}
+
+function setWon(){
+    let dWon = document.createElement('div')
+    dWon.classList.add('point')
+    dWon.style.opacity = 0.8
+    document.querySelector('.won').appendChild(dWon)
+    setTimeout(() => document.querySelector('.won').innerHTML='', 100)
+}
+
+function setScore(score){
+    document.querySelector('.score').innerHTML = score
+}
+
+function shuffle(array){
+    let currentIndex = array.length, temporaryValue, randomIndex
+
+    while (0 !== currentIndex){
+        randomIndex = Math.floor(Math.random()*currentIndex)
+        currentIndex -= 1
+
+        temporaryValue = array[currentIndex] 
+        array[currentIndex] = array[randomIndex]
+        array[randomIndex]= temporaryValue
+    }
+    return array
+}
+
